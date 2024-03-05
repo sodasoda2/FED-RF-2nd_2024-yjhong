1289bd4de962ffff8d3087d781fe7e11f31e7ecb
yjhong <qbru@naver.com>
2024-03-05 20:26:35 +0900

    3월5일
    1. 자바스크립트 학습
    2. nav 학습
    3. 1차 프로젝트 레이아웃 만들기

---
+++ 003.JS학습/react.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const react = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default react;
+
+const react = () => {
+    return (
+        <div>
+            테스트
+        </div>
+    )
+}
