bb668dda267b8c85639c3eba75d936a1cf79f98b
yjhong <qbru@naver.com>
2024-04-02 18:15:03 +0900

    4월 2일

--- 001.HTML학습/07.ShoppingMall_PJ/js/main0.js
+++ 001.HTML학습/07.ShoppingMall_PJ/js/main0.js
@@ -6,10 +6,11 @@ const qsa = (x) => document.querySelectorAll(x);
 
 // addEvent 함수
 // ele - 요소, evt - 이벤트, fn - 함수
-const addEvt = (ele, evt, fn) => ele.addEventListener(evt, fn);
+const addEvt = 
+(ele, evt, fn) => ele.addEventListener(evt, fn);
 
 // HTML태그 로딩후 loadFn함수 호출! ///
-addEvt(window, "DOMContentLoaded", loadFn);
+addEvt(window,"DOMContentLoaded", loadFn);
 
 /***************************************************** 
     [ 슬라이드 이동 기능정의 ]
@@ -40,97 +41,106 @@ addEvt(window, "DOMContentLoaded", loadFn);
     기능: 로딩 후 버튼 이벤트 및 기능구현
 ******************************************/
 function loadFn() {
-  console.log("로딩완료!");
+    console.log("로딩완료!");
+    
 
-  // [대상선정]
-  // 이동버튼 대상:  .abtn
-  const abtn = qsa(".abtn");
-  // 변경대상 : #slide
-  const slide = qs("#slide");
+    // [대상선정]
+    // 이동버튼 대상:  .abtn
+    const abtn = qsa('.abtn');
+    // 변경대상 : #slide
+    const slide = qs('#slide');
+    // 블릿대상:
+    const indic = 
+    document.querySelectorAll('.indic li');
+    // console.log(abtn,slide,indic);
 
-  // 블릿대상:
-  const indic = document.querySelectorAll(".indic li");
-  console.log(abtn, slide, indic);
+    // 왼쪽 버튼 처음에 숨기기
+    abtn[0].style.display = 'none';
 
-  // 왼쪽 버튼 처음에 숨기기
-  abtn[0].style.display = "none";
+    // 슬라이드 순번 전역변수
+    let snum = 0;
 
-  // 슬라이드 순번 전역변수
-  let snum = 0;
+    // 2. 버튼을 모두 이벤트 설정하기
+    for(let x of abtn){
+        x.onclick = goSlide;
+    } /// for of ///
 
-  // 2. 버튼을 모두 이벤트 설정하기
-  for (let x of abtn) {
-    x.onclick = goSlide;
-  } /// for of ///
+    // 2. 오른쪽 버튼 클릭시 기능구현
+    // abtn[1].onclick = ()=>{
+    // };
 
-  // 2. 오른쪽 버튼 클릭시 기능구현
-  // abtn[1].onclick = ()=>{
-  // };
-
-  // 3. 왼쪽 버튼 클릭시 기능구현
-  // abtn[0].onclick = ()=>{
-  // };
-
-  /****************************************** 
+    // 3. 왼쪽 버튼 클릭시 기능구현
+    // abtn[0].onclick = ()=>{
+    // };
+    
+    /****************************************** 
      함수명: goSlide
      기능: 슬라이드 이동
      ******************************************/
-  function goSlide() {
-    // 1.오른쪽 버튼인 .ab2인가?
-    let isRbtn = this.classList.contains("ab2");
-    // [classList 객체의 contains() 메서드]
-    // -> 해당요소의 특정 클래스인지 여부를 리턴함
-    // 해당클래스가 있으면 true, 없으면 false
+    function goSlide(){
+        // 1.오른쪽 버튼인 .ab2인가?
+        let isRbtn = 
+        this.classList.contains('ab2');
+        // [classList 객체의 contains() 메서드]
+        // -> 해당요소의 특정 클래스인지 여부를 리턴함
+        // 해당클래스가 있으면 true, 없으면 false
 
-    // 함수호출확인
-    console.log("나 슬라이드야~!", this, isRbtn);
-    // this는 호출한 버튼 자신
 
-    // 2. 오른쪽버튼이면 ++, 아니면 --
-    // 슬라이드 순번 증감
-    isRbtn ? snum++ : snum--;
+        // 함수호출확인
+        console.log('나 슬라이드야~!',this,isRbtn);
+        // this는 호출한 버튼 자신
+        
+        // 2. 오른쪽버튼이면 ++, 아니면 --
+        // 슬라이드 순번 증감
+        isRbtn? snum++ : snum--;
 
-    // 3. 한계값 설정하기
-    // 한계값일때 각 버튼 숨기기
-    // 3-1. 오른쪽 버튼일 경우 0보다 작으면 숨기기
-    // snum은 0으로 고정!
-    if (snum < 0) {
-      snum = 0;
-    } /// if ///
-    // snum은 4로 고정!
-    else if (snum > 4) {
-      snum = 4;
-    } /// else if ///
+        // 3. 한계값 설정하기
+        // 한계값일때 각 버튼 숨기기
+        // 3-1. 오른쪽 버튼일 경우 0보다 작으면 숨기기
+        // snum은 0으로 고정!
+        if(snum<0){
+            snum = 0;
+        } /// if ///
+        // snum은 4로 고정!
+        else if(snum>4){
+            snum = 4;
+        } /// else if ///
+        
+        // 4. 마지막 구역에서 버튼 숨기기
+        if(snum === 0 || snum === 4){
+            // 해당버튼 숨기기
+            this.style.display = 'none';            
+        } /// if ///
+        else{
+            // 버튼 다시 보이기
+            for(let x of abtn){
+                x.style.display = 'block'; 
+            } /// for of ///
 
-    // 4. 마지막 구역에서 버튼 숨기기
-    if (snum === 0 || snum === 4) {
-      // 해당버튼 숨기기
-      this.style.display = "none";
-    } /// if ///
-    else {
-      // 버튼 다시 보이기
-      for (let x of abtn) {
-        x.style.display = "block";
-      } /// for of ///
-    } /// else ///
+        } /// else ///
+        
 
-    console.log("이동%:", -100 * snum + "%");
+       console.log('이동%:',(-100*snum)+'%');
+    
+       slide.style.left = (-100*snum)+'%';
+       slide.style.transition = '.6s ease-in-out';
 
-    slide.style.left = -100 * snum + "%";
-    slide.style.transition = ".6s ease-in-out";
+       // 5. 블릿표시 구현하기
+       // 모든 클래스 on지우기+현재 순번 클래스 넣기
+       indic.forEach((ele,idx)=>{
+        // ele - 각각의 li, idx - 각각의 순번
+        if(idx===snum){ // 현재순번 on넣기
+            ele.classList.add('on');
+        } /// if ///
+        else{ // 나머지는 on빼기
+            ele.classList.remove('on');
+        } /// else ///
 
-    // 5. 블릿표시 구현하기
-    // 모든 클래스 on지우기+현재 순번 클래스 넣기
-    indic.forEach((ele, idx) => {
-      // ele - 각각의 li, idx - 각각의 순번
-      if (idx === snum) {
-        ele.classList.add("on");
-      } //// if ////
-      else {
-        ele.classList.remove("on");
-      } //// else ////
-    }); ////// forEach //////
-  } ///////////// goSlide 함수 ////////////////
-  /////////////////////////////////////////////
+       }); ///// forEach /////
+
+   } ///////////// goSlide 함수 ////////////////
+   /////////////////////////////////////////////
+
+    
 } //////////////// loadFn 함수 ///////////////
 /////////////////////////////////////////////
--- 001.HTML학습/07.ShoppingMall_PJ/js/main1.js
+++ 001.HTML학습/07.ShoppingMall_PJ/js/main1.js
@@ -61,28 +61,34 @@ function loadFn() {
   const slide = qs("#slide");
   // 블릿버튼 : .indic
   let indic = document.querySelector(".indic");
+  // console.log(abtn,slide);
 
-  //////////////// 초기세팅하기 /////////////////
+  //////////// 초기셋팅하기 ////////
   // 5개의 슬라이드와 블릿을 만들어준다!
   for (let i = 0; i < 5; i++) {
     // 슬라이드 넣기
     slide.innerHTML += `
     <li data-seq="${i}">
-      <img src="images/slide0${i + 1}.jpg" alt="slide" />`;
+        <img 
+        src="images/slide0${i + 1}.jpg"         
+        alt="slide">
+    </li>    
+    `;
     // 블릿 넣기
     indic.innerHTML += `
     <li ${i === 0 ? 'class="on"' : ""}>
-      <img src="images/dot1.png" alt="흰색" />
-      <img src="images/dot2.png" alt="회색" />
-    </li>`;
-  } ////// for /////////
+        <img src="images/dot1.png" alt="흰색">
+        <img src="images/dot2.png" alt="회색">
+    </li>
+    `;
+  } ////// for ////////
 
-  // li를 생성한 후 그 li를 다시 수집한다!
+  // li를 생성한 후 그 li다시 수집한다!
   // 블릿의 li까지 수집! indic 변수
-  indic = document.querySelectorAll(".indic li");
+  indic = document.querySelectorAll('.indic li');
 
   // 슬라이드 순번 전역변수
-  let snum = 0;
+  // let snum = 0; -> 불필요!
 
   // 2. 버튼을 모두 이벤트 설정하기
   for (let x of abtn) {
@@ -103,26 +109,26 @@ function loadFn() {
   /****************************************** 
      함수명: goSlide
      기능: 슬라이드 이동
-  ******************************************/
-  function goSlide(evt, sts = true) {
+     ******************************************/
+  function goSlide(evt,sts=true){
     // evt-이벤트객체전달 : PointerEvent{}
     // sts-버튼클릭인지 자동호출인지 구분하는 변수
     // -> true면 버튼클릭, false면 자동호출로 구분
     // -> 버튼클릭시엔 아무것도 안보내므로 기본값
     // true가 할당되어 적용됨!
-    // -> 만약 전달값이 없으면 기본값으로 세팅함!
-    // -> ES6문법에서 전달변수 초기값 주기 문법생김
+    // -> 만약 전달값이 없으면 기본값으로 셋팅함!
+    // -> ES6문법에서 전달변수 초기값 주기 문법생김!
 
     // 함수를 호출시에 아무값도 보내지 않으면
     // 함수의 전달변수 하나를 쓸 경우 또는
     // 여러전달변수중 첫번째 변수는 이벤트객체가
     // 전달된다!
-    console.log("전달변수하나:", evt, sts);
+    console.log('전달변수:',evt,sts);
 
-    // 만약 버튼 클릭일 경우 인터벌지우기함수호출!
-    if (sts) {
+    // 만약 버튼 클릭일 경우 인터발지우기함수호출!
+    if(sts) {
       clearAuto();
-    } ////// if //////
+    } ///// if ////
 
     // 광클금지 설정하기 ///////////
     // 클릭신호를 막아서 못들어오게 하고
@@ -135,10 +141,11 @@ function loadFn() {
     /////////////////////////////////////
 
     // 1.오른쪽 버튼인 .ab2인가?
-    let isRbtn = sts ? this.classList.contains("ab2") : true;
-    // 2. sts값이 true냐? 맞으면 버튼을 클릭한 것이므로
-    // this키워드에 의한 클래스 .ab2 존재여부를 물어라
-    // false냐? 맞으면 무조건 true를 할당해라!
+    let isRbtn = 
+    sts?this.classList.contains("ab2"):true;
+    // sts값이 true냐? 맞으면 버튼을 클릭한 것이므로
+    // this키워드에 의한 클래스 .ab2 존재여부를 물어라!
+    // false냐? 맞으면 무조건 true값을 할당해라!
     // 왜? 자동넘김은 오른쪽버튼 클릭한 방향으로 가야하니까!
 
     // [classList 객체의 contains() 메서드]
@@ -206,41 +213,50 @@ function loadFn() {
       }, 0);
     } /// else ///
 
-    // 3. 블릿을 위해 읽어올 슬라이드 순번 구하기
-    // 현재순번은 몇번째 슬라이드의
+    // 3.블릿을 위해 읽어올 슬라이드 순번 구하기
+    // 현재순번은 몇번째 슬라이드의 
     // data-seq속성값이다!
-    // 오른쪽버튼은 이동후 잘라내므로 두번째순번[1]
-    // 왼쪽버튼은 먼저 앞에 붙이고 이동하므로 첫번째순번[0]
+    // 오른쪽버튼은 
+    // 이동후 잘라내므로 두번째순번[1]
+    // 왼쪽버튼은 먼저 앞에 붙이고 
+    // 이동하므로 첫번째순번[0]
+    let seq = 
+    slide.querySelectorAll('li')[isRbtn?1:0]
+    .getAttribute('data-seq');
+    console.log('블릿이 읽어올 슬순번:',seq,
+    '/데이터형:',typeof seq);
+    // string - 문자형, number - 숫자형
 
-    let seq = slide
-      .querySelectorAll("li")
-      [isRbtn ? 1 : 0].getAttribute("data-seq");
-    console.log("블릿이 읽어올 순번:", seq, "/데이터형:", typeof seq);
+    // 4. 블릿변경하기 ///////////
+    // 모든 클래스 on지우기+현재 순번 클래스 넣기
+    indic.forEach((ele,idx)=>{
+        // ele - 각각의 li, idx - 각각의 순번
+        if(idx==seq){ // 현재순번 on넣기
+            // ==으로 비교해야 결과가 나옴
+            // data-seq 속성은 문자형숫자이므로!
+            // ===은 형까지 비교하기때문에 안나옴!
+            ele.classList.add('on');
+        } /// if ///
+        else{ // 나머지는 on빼기
+            ele.classList.remove('on');
+        } /// else ///
 
-    // 4. 블릿변경하기
-    indic.forEach((ele, idx) => {
-      // ele - 각각의 li, idx - 각각의 순번
-      console.log("seq", seq);
-      if (idx == seq) {
-        // ==으로 비교해야 결과가 나옴
-        // data-seq 속성은 문자형숫자이므로!
-        // ===은 형까지 비교하기때문에 안나옴!
-        ele.classList.add("on");
-      } //// if ////
-      else {
-        ele.classList.remove("on");
-      } //// else ////
-    }); ////// forEach //////
+       }); ///// forEach /////
+
+
   } ///////////// goSlide 함수 ////////////////
   /////////////////////////////////////////////
+
+  // [ 자동넘김 셋팅 구역 ] //////////////
+  // 인터발용 변수(지울목적)
   let autoI;
   // 타임아웃용 변수(지울목적)
   let autoT;
   // 자동넘김호출함수 최초호출하기
   autoSlide();
 
-  // [자동넘김호출 함수]
-  function autoSlide() {
+  // [ 자동넘김호출함수 ] /////
+  function autoSlide(){
     // setInterval(함수,시간)
     // - 일정시간간격으로 함수를 호출
     // clearInterval(인터발변수)
@@ -251,21 +267,27 @@ function loadFn() {
       // false값을 쓰고
       // 두번째 전달값은 자동호출임을 알리는 변수이므로
       // false값을 전달한다!
-      goSlide(false, false);
+      goSlide(false,false);
     }, 3000);
-  } ///////// autoSlide 함수 /////////////////
-  /////////// [ 인터발 지우기함수 ] ///////////
+
+  } /////// autoSlide 함수 /////////////
+
+  /// [ 인터발 지우기함수 ] ///////
   function clearAuto() {
     // 지우기 확인!
-    console.log("인터발지워!");
+    console.log('인터발 지워!');
     // 1.인터발 지우기
     clearInterval(autoI);
-    // 2.타임아웃  지우기 : 실행쓰나미 방지!!!
+    // 2.타임아웃 지우기 : 실행쓰나미 방지!!!
     clearTimeout(autoT);
-    // 5초후 아무작동도 안하면 다시 인터발호출
+    // 3.5초후 아무작동도 안하면 다시 인터발호출
     autoT = setTimeout(() => {
       autoSlide();
     }, 5000);
-  }
+
+  } ////////// clearAuto ////////////
+
+
+
 } //////////////// loadFn 함수 ///////////////
 /////////////////////////////////////////////
